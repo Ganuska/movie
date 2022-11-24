0a7310e8ca59825084fa7288a240fc68916de7f6
Zoran <101511908+Ganuska@users.noreply.github.com>
2022-11-24 21:20:46 +0100

    dodana funkcija 'load more'

--- src/components/movieCard/Card.js
+++ src/components/movieCard/Card.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./card.module.scss";
-const Card = ({ title, description, language, src, rating }) => {
+const Card = ({ title, description, language, src, rating, year }) => {
+  const [source, setSource] = useState();
+  useEffect(() => {
+    if (src !== null) {
+      setSource("https://image.tmdb.org/t/p/w500/" + src);
+    } else {
+      setSource("./assets/placeholder.jpg");
+    }
+  }, [source, src]);
+
   return (
     <div className={styles.container}>
-      <img
-        className={styles.bannerImg}
-        src={"https://image.tmdb.org/t/p/w500/" + src}
-        alt="movie banner"
-      />
+      <img className={styles.bannerImg} src={source} alt="movie banner" />
       <h3>{title}</h3>
       <p>{language}</p>
       <p>{description}</p>
+      <p>{year}</p>
       <div className={styles.ratingCont}>
         <p>{rating}</p>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,21 +4,32 @@ import Card from "../components/movieCard/Card";
 import styles from "./home.module.scss";
 const Home = () => {
   const [movies, setMovies] = useState([]);
-
+  const [page, setPage] = useState(2);
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`
       );
       console.log(result.data);
-      setMovies(result.data);
+      setMovies(result.data.results);
     };
     fetchData();
   }, []);
 
+  const fetchMore = async () => {
+    const result = await axios(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+    );
+    console.log(result.data.results);
+    setMovies((old) => {
+      return [...old, ...result.data.results];
+    });
+    setPage((old) => old + 1);
+  };
+
   return (
     <div className={styles.content}>
-      {movies.results?.map((movie) => {
+      {movies?.map((movie) => {
         return (
           <Card
             key={movie.id}
@@ -27,11 +38,14 @@ const Home = () => {
             language={movie.original_language}
             rating={movie.vote_average}
             description={movie.overview}
+            year={movie.release_date}
           />
         );
       })}
 
-      <button className={styles.btn}>load</button>
+      <button onClick={fetchMore} className={styles.btn}>
+        load
+      </button>
     </div>
   );
 };
